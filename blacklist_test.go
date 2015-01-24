c11e2f60d79c7041495896d1cbed75c9175a6078
Attila Oláh <attilaolah@gmail.com>
2015-01-24 20:53:21 +0100

    add a blacklist test

--- blacklist_test.go
+++ blacklist_test.go
@@ -6,20 +6,42 @@ import (
 	"github.com/gophergala/dnsp"
 )
 
-func TestIsAllowed(t *testing.T) {
+func TestIsAllowedWhite(t *testing.T) {
 	t.Parallel()
 
 	s := dnsp.NewServer(dnsp.Options{
 		White: true,
 	})
-	s.Whitelist("google.com")
 	s.Whitelist("github.com")
+	s.Whitelist("google.com")
 
 	for host, ok := range map[string]bool{
 		"blocked.net.": false,
 		"example.com.": false,
 		"github.com.":  true,
 		"google.com.":  true,
+	} {
+		if act := s.IsAllowed(host); ok != act {
+			t.Errorf("expected s.IsAllowed(%q) to be %v, got %v", host, ok, act)
+		}
+	}
+}
+
+func TestIsAllowedBlack(t *testing.T) {
+	t.Parallel()
+
+	s := dnsp.NewServer(dnsp.Options{
+		White: false,
+	})
+	s.Blacklist("doubleclick.net")
+	s.Blacklist("porn.com")
+
+	for host, ok := range map[string]bool{
+		"doubleclick.net.": false,
+		"example.com.":     true,
+		"github.com.":      true,
+		"google.com.":      true,
+		"porn.com.":        false,
 	} {
 		if act := s.IsAllowed(host); ok != act {
 			t.Errorf("expected s.IsAllowed(%q) to be %v, got %v", host, ok, act)
