2105e0e430193fa27d7653b7935ec23a5e5a9a3a
Attila Oláh <attilaolah@gmail.com>
2015-01-24 17:08:11 +0100

    implement a full proxy with blacklist support

--- options.go
+++ options.go
@@ -2,5 +2,9 @@ package dnsp
 
 // Options can be passed to NewServer().
 type Options struct {
+	// Bind is the local address to listen on.
 	Bind string
+	// Server is the address of the upstream DNS server to proxy to.
+	// If empty, all queries will fail.
+	Server string
 }
