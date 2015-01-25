1a17dcab44d5c94ea84404aa1f392aab0c339a37
Attila Oláh <attilaolah@gmail.com>
2015-01-25 07:16:03 +0100

    update docs

--- doc_test.go
+++ doc_test.go
@@ -10,8 +10,10 @@ func Example() {
 	// Create a server that listens on :1053, on all interfaces.
 	// DNS queries will be proxied to Google's public nameservers.
 	s, err := dnsp.NewServer(dnsp.Options{
-		Bind:    ":1035",
+		Bind:    ":1053",
 		Resolve: []string{"8.8.4.4", "8.8.8.8"},
+		// Block hosts listed in a community-managed file:
+		Blacklist: "http://hosts-file.net/download/hosts.txt",
 	})
 	if err != nil {
 		log.Fatal(err)
