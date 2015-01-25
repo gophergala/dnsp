6c4d8934b0d295cfd2f3f74213f443435dfe5733
Ryan Leavengood <leavengood@gmail.com>
2015-01-25 17:10:30 -0500

    Simplify the API, add a lot of JavaScript

--- http.go
+++ http.go
@@ -42,50 +42,33 @@ func (h *httpServer) mode(w http.ResponseWriter, r *http.Request, _ httprouter.P
 }
 
 func (h *httpServer) publicListCount(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprintf(w, "{\"count\":%d}\n", 1234) //h.server.publicListCount())
+	fmt.Fprintf(w, "{\"count\":%d}\n", 1234) // TODO: h.server.publicListCount())
 }
 
-func (h *httpServer) list(white bool) httprouter.Handle {
-	return func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-		// var urls []string
-		urls := []string{"1", "2", "3"}
-		if white {
-			// urls = {"1", "2"} //h.server.whitelist()
-		} else {
-			// urls = {"3", "4"} //h.server.blacklist()
-		}
+func (h *httpServer) list(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	// TODO: Get the real list
+	urls := []string{"1", "2", "3"}
 
-		encoder := json.NewEncoder(w)
-		encoder.Encode(urls)
-	}
+	encoder := json.NewEncoder(w)
+	encoder.Encode(urls)
 }
 
-func (h *httpServer) add(white bool) httprouter.Handle {
-	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-		url := ps.ByName("url")
-		if white {
-			// h.server.addToWhitelist(url)
-		} else {
-			// h.server.addToBlacklist(url)
-		}
+func (h *httpServer) add(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	url := ps.ByName("url")
 
-		// TODO: response?
-		fmt.Fprintf(w, "{add:%q}\n", url)
-	}
+	h.server.addHostEntry(url)
+
+	// TODO: different response?
+	fmt.Fprintf(w, "{added:%q}\n", url)
 }
 
-func (h *httpServer) remove(white bool) httprouter.Handle {
-	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-		url := ps.ByName("url")
-		if white {
-			// h.server.removeFromWhitelist(url)
-		} else {
-			// h.server.removeFromBlacklist(url)
-		}
+func (h *httpServer) remove(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	url := ps.ByName("url")
+	// TODO
+	// h.server.removeHostEntry(url)
 
-		// TODO: response?
-		fmt.Fprintf(w, "{remove:%q}\n", url)
-	}
+	// TODO: different response?
+	fmt.Fprintf(w, "{removed:%q}\n", url)
 }
 
 func RunHTTPServer(host string, s *Server) {
@@ -101,19 +84,12 @@ func RunHTTPServer(host string, s *Server) {
 	// Gets the count for the public blacklist
 	router.GET("/blacklist/public", h.publicListCount)
 
-	// Gets the personal blacklist
-	router.GET("/blacklist", h.list(false))
-	// Adds a new URL to the blacklist
-	router.PUT("/blacklist/:url", h.add(false))
-	// Removes a URL from the blacklist
-	router.DELETE("/blacklist/:url", h.remove(false))
-
-	// Gets the personal whitelist
-	router.GET("/whitelist", h.list(true))
-	// Adds a new URL to the whitelist
-	router.PUT("/whitelist/:url", h.add(true))
-	// Removes a URL from the whitelist
-	router.DELETE("/whitelist/:url", h.remove(true))
+	// Gets the personal list
+	router.GET("/list", h.list)
+	// Adds a new URL to the list
+	router.PUT("/list/:url", h.add)
+	// Removes a URL from the list
+	router.DELETE("/list/:url", h.remove)
 
 	log.Fatal(http.ListenAndServe(host, router))
 }
